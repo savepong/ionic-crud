49b8b89f80266ae33c61b5f8c962e8122c6a56f6
Savepong <savepong.com@gmail.com>
2019-03-05 13:08:05 +0700

    Create A Page for Display Classroom Details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,9 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
+  },
+  {
+    path: 'detail/:id', loadChildren: './detail/detail.module#DetailPageModule'
   }
 ];
 
